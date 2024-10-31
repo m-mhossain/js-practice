6171079ddf82ea869a4ac921cb094007cdcd16cb
Musharof Hossain <musharof.caab@gmail.com>
2024-10-31 17:27:05 +0600

    ES6 CLASS-OBJECT

--- index.js
+++ index.js
@@ -49,22 +49,59 @@
 
 //HAS()
 
-let myMap = new Map()
+// let myMap = new Map()
 
-myMap.set('key1', 'Bangladesh')
-myMap.set('key2', 'India')
-myMap.set('key3', 'Srilanka')
-myMap.set('key4', 'Maldiv')
-myMap.set('key5', 'Nepal')
-myMap.set('key6', 'Thailand')
+// myMap.set('key1', 'Bangladesh')
+// myMap.set('key2', 'India')
+// myMap.set('key3', 'Srilanka')
+// myMap.set('key4', 'Maldiv')
+// myMap.set('key5', 'Nepal')
+// myMap.set('key6', 'Thailand')
 
-if(myMap.has('key7')){
- console.log("Yes, it is available")
-}else {
-    console.log("No, it is not available")
+// if(myMap.has('key7')){
+//  console.log("Yes, it is available")
+// }else {
+//     console.log("No, it is not available")
+// }
+
+//=======================================================
+//ES6 CLASS AND OBJECT
+
+// class myClass {
+//     myFun1(){
+//         console.log('Hello, I am from function inside class.');        
+//     }
+//     myFun2(){
+//         console.log('Hello, I am from function inside class.');        
+//     }
+//     myFun3(){
+//         console.log('Hello, I am from function inside class.');        
+//     }
+// }
+
+// var obj = new myClass;
+// obj.myFun1();
+// obj.myFun2();
+// obj.myFun3();
+
+//PARAMETERISED FUNCTION
+
+class myClass {
+    myFun1(name){
+        console.log(name);
+    }
+    myFun2(name){
+        console.log(name);
+    }
+    myFun3(name){
+        console.log(name);
+    }
 }
 
-
+var Obj = new myClass;
+Obj.myFun1('Bangladesh');
+Obj.myFun2('India');
+Obj.myFun3('Thailand');
 
 
 
